533fa6c2f12c253a08ed469c28d66e68bea20b76
yadharis-perdomou <123768363+yadharis-perdomou@users.noreply.github.com>
2023-02-10 14:01:19 -0500

    appjss, imagenes

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {});
+const cardAdj = [
+  { name: "1", img: "imagenes/1.png" },
+  { name: "2", img: "imagenes/2.png" },
+  { name: "3", img: "imagenes/3.png" },
+  { name: "4", img: "imagenes/4.png" },
+  { name: "5", img: "imagenes/5.png" },
+  { name: "6", img: "imagenes/6.png" },
+  { name: "7", img: "imagenes/7.png" },
+  { name: "8", img: "imagenes/8.png" }
+];
+
+const cuadricula = document.querySelector(".cuadricula");
+const resultado = document.querySelector("#resultado");
+var cartasEscogidas = [];
+var cartasEscogidasId = [];
+var cartasGanadas = [];
+});
